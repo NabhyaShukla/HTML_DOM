6a04f26165459d61683d8991264936fdee217f33
NabhyaShukla <nabhyashukla69@gmail.com>
2026-03-28 21:44:27 +0530

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function myFunction(){
-    var x = 5
-    document.getElementById("demo").innerHTML = x * x;
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("square-heading").classList.add("display-none")
+})
+
+
+
+
+
+
+function myFunction(event){
+    event.pereventDefault();
+    document.getElementById("square-heading").classList.add("display-visible")
+    
+
+    let inputValue = document.getElementById("num").value;
+
+    inputValue = Number(inputValue)
+    document.getElementById("demo").innerHTML = inputValue * inputValue;
+    document.getElementById("demo").classList.add("square")
 }
